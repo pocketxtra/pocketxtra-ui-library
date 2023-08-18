0d9974e707d94e1fc5536a48f85d6ebf959d7893
tabassum2507 <10tabassum2@gmail.com>
2023-08-18 16:08:57 +0530

    cards

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import './components/buttons/index' // import all buttons component here to the entry point for the npm package
+import '../src/components/cards/index' // import all buttons component here to the entry point for the npm package
---
+++ src/interface/buttons/cards/DashBoardInfoCardInterface.ts
@@ -0,0 +1,11 @@
+export interface DashBoardInfoCardInterface{
+    width?: number,
+    height?: number,
+    EventName?: string,
+    color?: string,
+    EventDate?: string,
+    colorDark?: string,
+    CompanyName?: string,
+    borderRadius?: number,
+    
+}
