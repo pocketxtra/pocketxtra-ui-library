dc216d3b5b21caf9c44e2acf3864df3b5ff7a3e3
tabassum2507 <10tabassum2@gmail.com>
2023-08-22 12:02:33 +0530

    background color

--- src/components/navigationBar/navigationBar.tsx
+++ src/components/navigationBar/navigationBar.tsx
@@ -1,79 +1,79 @@
-import * as React from 'react';
-import { Text, View, StyleSheet  } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { Platform } from 'react-native';
-import { navigationBarInterface } from '../../interface/navigationBar/navigationBarInterface';
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { navigationBarInterface } from "../../interface/navigationBar/navigationBarInterface";
 
 const Tab = createBottomTabNavigator();
 
-export const NavigationBar : React.FC<navigationBarInterface> = ({
+export const NavigationBar: React.FC<navigationBarInterface> = ({
   tabScreens,
   tabBarStyle,
   tabBarActiveTintColor = '#B0E50E',
-  tabBarInactiveTintColor = 'gray',
-  backgroundColor = 'red',
+  tabBarInactiveTintColor = '#ccc',
+  iconColor = '#B0E50E',
+  createIconSize =50
 }) => {
-
   const mergedTabBarStyle = StyleSheet.compose(
     styles.defaultTabBarStyle,
     tabBarStyle,
-    {
-      backgroundColor: backgroundColor,
-    }
-    
   );
 
   return (
     <NavigationContainer>
-    <Tab.Navigator
-       screenOptions={({ route }) => ({
-        tabBarStyle: mergedTabBarStyle,
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarStyle: mergedTabBarStyle,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-          const selectedTab = tabScreens.find((tab) => tab.name === route.name);
-  
-          if (selectedTab) {
-            iconName = focused
-              ? `${selectedTab.icon}-sharp`
-              : `${selectedTab.icon}-outline`;
-  
-            if (selectedTab.name === 'Create') {
-              return (
-                <View>
-                  <Ionicons name={selectedTab.icon} size={50} color="#B0E50E" />
-                </View>
-              );
+            const selectedTab = tabScreens.find(
+              (tab) => tab.name === route.name
+            );
+
+            if (selectedTab) {
+              iconName = focused
+                ? `${selectedTab.icon}-sharp`
+                : `${selectedTab.icon}-outline`;
+
+              if (selectedTab.name === "Create") {
+                return (
+                  <View>
+                    <Ionicons
+                      name={selectedTab.icon}
+                      size={createIconSize}
+                      color= {iconColor}
+                    />
+                  </View>
+                );
+              }
             }
-          }
-  
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: tabBarActiveTintColor,
-        tabBarInactiveTintColor: tabBarInactiveTintColor,
-      })}
-    >
-      {tabScreens.map(({ name, component, icon, options }) => (
-        <Tab.Screen
-          key={name}
-          name={name}
-          component={component}
-          options={options}
-          tabBarIcon={({ focused, color, size }) => (
-            <Ionicons
-              name={focused ? `${icon}-sharp` : `${icon}-outline`}
-              size={size}
-              color={color}
-            />
-          )}
-        />
-      ))}
-    </Tab.Navigator>
-  </NavigationContainer>
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: tabBarActiveTintColor,
+          tabBarInactiveTintColor: tabBarInactiveTintColor,
+        })}
+      >
+        {tabScreens.map(({ name, component, icon, options }) => (
+          <Tab.Screen
+            key={name}
+            name={name}
+            component={component}
+            options={options}
+            tabBarIcon={({ focused, color, size }) => (
+              <Ionicons
+                name={focused ? `${icon}-sharp` : `${icon}-outline`}
+                size={size}
+                color={color}
+              />
+            )}
+          />
+        ))}
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   defaultTabBarStyle: {
@@ -81,15 +81,6 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 50,
     paddingBottom: 20,
     paddingTop: 20,
-    ...Platform.select({
-      ios: {
-        // backgroundColor: '#101318',
-      },
-      android: {
-        elevation: 8,
-        // backgroundColor: '#101318',
-      },
-    }),
+    backgroundColor: '#000'
   },
 });
-
--- src/interface/navigationBar/navigationBarInterface.ts
+++ src/interface/navigationBar/navigationBarInterface.ts
@@ -1,25 +1,27 @@
 export interface navigationBarInterface {
-    tabScreens?: TabScreen[];
-    tabBarStyle?: TabBarStyle; 
-    tabBarActiveTintColor?: string;
-    tabBarInactiveTintColor?: string;
+    tabScreens?: TabScreen[],
+    tabBarStyle?: TabBarStyle,
+    tabBarActiveTintColor?: string,
+    tabBarInactiveTintColor?: string,
     height?: number,
-    backgroundColor: string;
+    iconColor?: string,
+    createIconSize?: number
 }
 
 interface TabScreen {
-    name?: string;
-    component?: React.FC;
-    icon: string;
+    name?: string,
+    component?: React.FC,
+    icon: string,
     options?: {
         tabBarLabel?: () => null;
-    };
+    },
 }
 
 interface TabBarStyle {
-    borderTopLeftRadius?: number;
-    borderTopRightRadius?: number;
-    paddingBottom?: number;
-    paddingTop?: number;
-    height?: number;
+    borderTopLeftRadius?: number,
+    borderTopRightRadius?: number,
+    paddingBottom?: number,
+    paddingTop?: number,
+    height?: number,
+    backgroundColor?: string,
 }
