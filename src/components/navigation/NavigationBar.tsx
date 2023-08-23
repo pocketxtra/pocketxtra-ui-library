24f927a0947206e48bdbcd8c58d0bd61747459ca
Shubham <shubhambade3@gmail.com>
2023-08-23 16:04:45 +0530

    Updated Node module

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -8,7 +8,7 @@ import { Text, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import { NavigationBarInterface } from "../../interface/navigationBar/NavigationBarInterface";
+import { NavigationBarInterface } from "../../interface/navigation/NavigationBarInterface";
 import {Colors} from '../../theme/ColorsConstant'
 const Tab = createBottomTabNavigator();
 
